681481bf22b40a22b6e620282f2bdc818c6f8fd2
LontchiFrank <fodjof6@gmail.com>
2022-12-27 17:53:31 +0100

    router

---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+import RouteLoader from "./RouteLoader/RouteLoader.component";
+
+export { RouteLoader };
--- src/navigation/MainNavigation.jsx
+++ src/navigation/MainNavigation.jsx
@@ -1,11 +1,21 @@
 import React, { Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { RouteLoader } from "../components";
+import { routes } from "./routes";
 
 function MainNavigation() {
   return (
     <Router>
-      <Suspense>
-        <Routes></Routes>
+      <Suspense fallback={<RouteLoader />}>
+        <Routes>
+          {routes.map((route) => {
+            <Route
+              key={route.path}
+              path={route.path}
+              element={route.component}
+            />;
+          })}
+        </Routes>
       </Suspense>
     </Router>
   );
