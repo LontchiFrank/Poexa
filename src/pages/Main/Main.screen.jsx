f9190dfe2ab1755c5ff6617156ea77083ea3ad8c
LontchiFrank <fodjof6@gmail.com>
2022-12-29 00:11:13 +0100

    added colors in buttons

--- src/pages/Main/Main.screen.jsx
+++ src/pages/Main/Main.screen.jsx
@@ -6,9 +6,17 @@ function Main() {
   return (
     <section className={`${styles.main_screen}`}>
       <div className=" md:container md:mx-auto px-28 pt-8 ">
-        <nav>
+        <nav className="flex justify-between">
           <div>
             <img src={logo} />
+          </div>
+          <div className="flex justify-between">
+            <button className="rounded outline  outline-black-500 p-2">
+              <p className="text-white"> Sign In</p>
+            </button>
+            <button className="rounded outline  outline-violet-500 p-2 ml-4">
+              <p className="text-violet"> Sign Up</p>
+            </button>
           </div>
         </nav>
       </div>
