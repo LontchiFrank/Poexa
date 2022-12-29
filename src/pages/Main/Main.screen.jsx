c7432893e47addfd1bb803376b6fd0815865ac4f
LontchiFrank <fodjof6@gmail.com>
2022-12-29 19:42:50 +0100

    responsive navbar

--- src/pages/Main/Main.screen.jsx
+++ src/pages/Main/Main.screen.jsx
@@ -5,45 +5,50 @@ import logo from "../../assert/PoeXa.png";
 function Main() {
   return (
     <section className={`${styles.main_screen}`}>
-      <div className=" md:container md:mx-auto px-28 pt-8 ">
-        <nav className="flex justify-between">
-          <div>
-            <img src={logo} />
+      <div className=" md:container md:mx-auto px-28 pt-8 sm:px-12">
+        <nav className="flex justify-between ">
+          <div className="flex justify-between items-center">
+            <span className="text-2xl cursor-pointer ">
+              <img src={logo} />
+            </span>
+            <span className="text-3xl cursor-pointer md:hidden black">
+              <ion-icon name="menu"></ion-icon>
+            </span>
           </div>
           <div className="flex justify-between">
-            <ul className="flex">
-              <li className="mx-4">
+            <ul className="md:flex md:items-center z-[-1] md:z-auto md:static absolute w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500">
+              <li className="mx-3 my-6 md:my-0">
                 <a
                   href="#"
                   className="text-xl hover:text-cyan-500 duration-500  "
-                  style={{ color: "rgba(22,13,61,0.6)", fontFamily: "In" }}
+                  style={{ color: "rgba(22,13,61,0.6)", fontSize: "16px" }}
                 >
                   Home
                 </a>
               </li>
-              <li className="mx-4">
+              <li className="mx-3 my-6 md:my-0">
                 <a
                   href="#"
                   className="text-xl hover:text-cyan-500 duration-500  "
-                  style={{ color: "rgba(22,13,61,0.6)" }}
+                  style={{ color: "rgba(22,13,61,0.6)", fontSize: "16px" }}
                 >
                   About
                 </a>
               </li>
-              <li className="mx-4">
+              <li className="mx-3 my-6 md:my-0">
                 <a
                   href="#"
                   className="text-xl hover:text-cyan-500 duration-500  "
-                  style={{ color: "rgba(22,13,61,0.6)" }}
+                  style={{ color: "rgba(22,13,61,0.6)", fontSize: "16px" }}
                 >
                   Contact
                 </a>
               </li>
-              <li className="mx-4">
+              <li className="mx-3 my-6 md:my-0">
                 <a
                   href="#"
                   className="text-xl hover:text-cyan-500 duration-500  "
-                  style={{ color: "rgba(22,13,61,0.6)" }}
+                  style={{ color: "rgba(22,13,61,0.6)", fontSize: "16px" }}
                 >
                   Service
                 </a>
