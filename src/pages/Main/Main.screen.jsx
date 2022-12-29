e4225b95525e1dc0bfb03b88d8cf39d645c0d924
LontchiFrank <fodjof6@gmail.com>
2022-12-29 12:49:10 +0100

    added the links

--- src/pages/Main/Main.screen.jsx
+++ src/pages/Main/Main.screen.jsx
@@ -11,12 +11,51 @@ function Main() {
             <img src={logo} />
           </div>
           <div className="flex justify-between">
-            <button className="rounded outline  outline-black-500 p-2">
-              <p className="text-white"> Sign In</p>
-            </button>
-            <button className="rounded outline  outline-violet-500 p-2 ml-4">
-              <p className="text-violet"> Sign Up</p>
-            </button>
+            <ul className="flex">
+              <li className="mx-4">
+                <a
+                  href="#"
+                  className="text-xl hover:text-cyan-500 duration-500  "
+                  style={{ color: "rgba(22,13,61,0.6)", fontFamily: "In" }}
+                >
+                  Home
+                </a>
+              </li>
+              <li className="mx-4">
+                <a
+                  href="#"
+                  className="text-xl hover:text-cyan-500 duration-500  "
+                  style={{ color: "rgba(22,13,61,0.6)" }}
+                >
+                  About
+                </a>
+              </li>
+              <li className="mx-4">
+                <a
+                  href="#"
+                  className="text-xl hover:text-cyan-500 duration-500  "
+                  style={{ color: "rgba(22,13,61,0.6)" }}
+                >
+                  Contact
+                </a>
+              </li>
+              <li className="mx-4">
+                <a
+                  href="#"
+                  className="text-xl hover:text-cyan-500 duration-500  "
+                  style={{ color: "rgba(22,13,61,0.6)" }}
+                >
+                  Service
+                </a>
+              </li>
+
+              <button className="rounded outline  outline-black-500 p-2">
+                <p className="text-white"> Sign In</p>
+              </button>
+              <button className="rounded outline  outline-violet-500 p-2 ml-4">
+                <p className="text-violet"> Sign Up</p>
+              </button>
+            </ul>
           </div>
         </nav>
       </div>
