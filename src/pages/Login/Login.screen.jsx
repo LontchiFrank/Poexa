3ffb28810aa0826c3ea86d7518ed892108929e02
LontchiFrank <fodjof6@gmail.com>
2023-02-16 12:42:16 +0100

    login created

--- src/pages/Login/Login.screen.jsx
+++ src/pages/Login/Login.screen.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Poexa from "../../assert/PoeXa.png";
 
 function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = formData;
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <section class="bg-violet-50 dark:bg-gray-900">
       <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -22,7 +37,11 @@ function Login() {
             <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
               Login to account
             </h1>
-            <form class="space-y-4 md:space-y-6" action="#">
+            <form
+              onSubmit={(e) => handleSubmit(e)}
+              class="space-y-4 md:space-y-6"
+              action="#"
+            >
               <div>
                 <label
                   for="email"
@@ -33,6 +52,8 @@ function Login() {
                 <input
                   type="email"
                   name="email"
+                  value={email}
+                  onChange={(e) => handleChange(e)}
                   id="email"
                   class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="name@company.com"
@@ -49,6 +70,8 @@ function Login() {
                 <input
                   type="password"
                   name="password"
+                  value={password}
+                  onChange={(e) => handleChange(e)}
                   id="password"
                   placeholder="••••••••"
                   class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
