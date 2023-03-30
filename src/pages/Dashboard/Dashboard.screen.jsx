d496cdcf6286672fcca6096962ea76c936605509
LontchiFrank <fodjof6@gmail.com>
2023-03-30 09:44:09 +0100

    show me

--- src/pages/Dashboard/Dashboard.screen.jsx
+++ src/pages/Dashboard/Dashboard.screen.jsx
@@ -242,7 +242,7 @@ function Dashboard() {
       </aside>
 
       <div class="p-4 sm:ml-64">
-        <div class="p-4 rounded-lg dark:border-gray-700 mt-14">
+        <div class="w-full flex flex-wrap">
           <div className={`${styles.poems} `}>
             {poems.map((item, key) => (
               <PoemCard
--- src/pages/Posts/Posts.screen.jsx
+++ src/pages/Posts/Posts.screen.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import Poexa from "../../assert/PoeXa.png";
 
 function Posts() {
+  const [info, setInfo] = useState([]);
   return (
     <section className={`${styles.main}`}>
       <nav class="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
@@ -211,7 +212,20 @@ function Posts() {
       </aside>
 
       <div class="p-4 sm:ml-64">
-        <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700"></div>
+        <div class="mt-16 ">
+          <div className="flex justify-end">
+            <button
+              type="button"
+              class="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+            >
+              Purple to Pink
+            </button>
+          </div>
+
+          <div className="flex justify-center items-center">
+            <div></div>
+          </div>
+        </div>
       </div>
     </section>
   );
