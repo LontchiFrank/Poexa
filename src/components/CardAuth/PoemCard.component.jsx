8a311feac28333d67ea4c89590413cc15b45bb33
LontchiFrank <fodjof6@gmail.com>
2023-04-17 18:44:22 +0100

    removed comments

--- src/components/CardAuth/PoemCard.component.jsx
+++ src/components/CardAuth/PoemCard.component.jsx
@@ -18,56 +18,10 @@ function PoemCard({ item, on, handleClick }) {
   const couleur = check[Math.floor(Math.random() * check.length)];
 
   return (
-    // <div
-    //   class={`${styles.card} max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl`}
-    // >
-    //   <div class="md:flex">
-    //     <div class="md:shrink-0">
-    //       <img
-    //         class="h-full w-full object-cover md:h-full md:w-48"
-    //         src={item.image}
-    //         alt="Modern building architecture"
-    //       />
-    //     </div>
-    //     <div class="px-8 pt-8 pb-2">
-    //       <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
-    //         {item.category}
-    //       </div>
-    //       <a
-    //         href="#"
-    //         class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
-    //       >
-    //         {item.title}
-    //       </a>
-    //       <p class="mt-2 text-slate-500 mb-8">
-    //         <LinesEllipsis
-    //           text={item.desc}
-    //           maxLine="2"
-    //           ellipsis="..."
-    //           trimRight
-    //           basedOn="letters"
-    //         />
-    //       </p>
-
-    //       <div className="flex justify-between">
-    //         <button
-    //           type="button"
-    //           onClick={() => handleClick(true, item)}
-    //           class="inline-block rounded border-2 border-indigo-300 px-6 pt-2 pb-[6px] text-xs font-medium uppercase leading-normal text-primary transition duration-150 ease-in-out hover:border-primary-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-primary-600 focus:border-primary-600 focus:text-primary-600 focus:outline-none focus:ring-0 active:border-primary-700 active:text-primary-700 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
-    //           data-te-ripple-init
-    //         >
-    //           View
-    //         </button>
-    //         <FiHeart style={{ fontSize: "20px" }} />
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
     <div class="max-w-sm w-full lg:max-w-full lg:flex">
       <div
         class="h-48 lg:h-auto lg:w-48 flex-none bg-cover  rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
         style={{ backgroundImage: `url(${item.image})` }}
-        // url(`${item.image}`)url(`${item.image}`)
         title="Woman holding a mug"
       ></div>
       <div class="border-r border-b border-l border-none bg-slate-50 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
