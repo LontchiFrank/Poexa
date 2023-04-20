ddc7c077fb9a20fd785b2c9d9259fe485b9cc97b
LontchiFrank <fodjof6@gmail.com>
2023-04-20 11:00:46 +0100

    added card

--- src/pages/Main/Main.screen.jsx
+++ src/pages/Main/Main.screen.jsx
@@ -111,7 +111,7 @@ function Main() {
               >
                 Latest Poems
               </p>
-              <div>
+              {/* <div>
                 {poems.length == 0 ? (
                   <div
                     className="grid grid-cols-1"
@@ -132,7 +132,146 @@ function Main() {
                     ))}
                   </div>
                 )}
+              </div> */}
+
+              <div className="flex">
+                <div class="max-w-sm w-full lg:max-w-full lg:flex">
+                  <div
+                    class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
+                    style={{
+                      backgroundImage:
+                        "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzn5z3GQKbI5FnB8UVcD4jRbwI3pYxoFTRzw&usqp=CAU')",
+                    }}
+                    title="Woman holding a mug"
+                  ></div>
+                  <div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+                    <div class="mb-8">
+                      <p class="text-sm text-gray-600 flex items-center">
+                        <svg
+                          class="fill-current text-gray-500 w-3 h-3 mr-2"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                        >
+                          <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
+                        </svg>
+                        Members only
+                      </p>
+                      <div class="text-gray-900 font-bold text-xl mb-2">
+                        Can coffee make you a better developer?
+                      </div>
+                      <p class="text-gray-700 text-base">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Voluptatibus quia, nulla! Maiores et perferendis
+                        eaque, exercitationem praesentium nihil.
+                      </p>
+                    </div>
+                    <div class="flex items-center">
+                      <img
+                        class="w-10 h-10 rounded-full mr-4"
+                        src="/img/jonathan.jpg"
+                        alt="Avatar of Jonathan Reinink"
+                      />
+                      <div class="text-sm">
+                        <p class="text-gray-900 leading-none">
+                          Jonathan Reinink
+                        </p>
+                        <p class="text-gray-600">Aug 18</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="max-w-sm w-full lg:max-w-full lg:flex">
+                  <div
+                    class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
+                    style={{
+                      backgroundImage:
+                        "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJeRLpcFr7al_URljs2Wd_4x-z8mdH2XOWcw&usqp=CAU')",
+                    }}
+                    title="Woman holding a mug"
+                  ></div>
+                  <div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+                    <div class="mb-8">
+                      <p class="text-sm text-gray-600 flex items-center">
+                        <svg
+                          class="fill-current text-gray-500 w-3 h-3 mr-2"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                        >
+                          <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
+                        </svg>
+                        Members only
+                      </p>
+                      <div class="text-gray-900 font-bold text-xl mb-2">
+                        Can coffee make you a better developer?
+                      </div>
+                      <p class="text-gray-700 text-base">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Voluptatibus quia, nulla! Maiores et perferendis
+                        eaque, exercitationem praesentium nihil.
+                      </p>
+                    </div>
+                    <div class="flex items-center">
+                      <img
+                        class="w-10 h-10 rounded-full mr-4"
+                        src="/img/jonathan.jpg"
+                        alt="Avatar of Jonathan Reinink"
+                      />
+                      <div class="text-sm">
+                        <p class="text-gray-900 leading-none">
+                          Jonathan Reinink
+                        </p>
+                        <p class="text-gray-600">Aug 18</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="max-w-sm w-full lg:max-w-full lg:flex">
+                  <div
+                    class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
+                    style={{
+                      backgroundImage:
+                        "url('https://images.unsplash.com/photo-1545912452-8aea7e25a3d3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGxhZHl8ZW58MHx8MHx8&w=1000&q=80')",
+                    }}
+                    title="Woman holding a mug"
+                  ></div>
+                  <div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+                    <div class="mb-8">
+                      <p class="text-sm text-gray-600 flex items-center">
+                        <svg
+                          class="fill-current text-gray-500 w-3 h-3 mr-2"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                        >
+                          <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
+                        </svg>
+                        Members only
+                      </p>
+                      <div class="text-gray-900 font-bold text-xl mb-2">
+                        Can coffee make you a better developer?
+                      </div>
+                      <p class="text-gray-700 text-base">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Voluptatibus quia, nulla! Maiores et perferendis
+                        eaque, exercitationem praesentium nihil.
+                      </p>
+                    </div>
+                    <div class="flex items-center">
+                      <img
+                        class="w-10 h-10 rounded-full mr-4"
+                        src="/img/jonathan.jpg"
+                        alt="Avatar of Jonathan Reinink"
+                      />
+                      <div class="text-sm">
+                        <p class="text-gray-900 leading-none">
+                          Jonathan Reinink
+                        </p>
+                        <p class="text-gray-600">Aug 18</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
+
               <div className="w-full flex justify-center items-center pt-20">
                 <button
                   type="button"
