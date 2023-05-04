43c3db06a3bfceed5200e47a9959a449ebf5d611
LontchiFrank <fodjof6@gmail.com>
2023-05-04 13:14:06 +0100

    fix the menu bar on landing page

--- src/components/Wrapper/Wrapper.component.jsx
+++ src/components/Wrapper/Wrapper.component.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Footer from "../footer/Footer.component";
 import logo from "../../assert/PoeXa.png";
 import { Link } from "react-router-dom";
+import {
+  FaAddressBook,
+  FaHandHolding,
+  FaHome,
+  FaRegistered,
+  FaSignInAlt,
+} from "react-icons/fa";
 
 function Wrapper({ children }) {
+  const [toggle, setToggle] = useState(false);
+  const handleToggler = () => {
+    setToggle(!toggle);
+  };
   return (
     <div>
       <div className="md:container xl:px-8 px-28 mx-auto  pt-8 max-[940px]:px-12  ">
@@ -12,9 +23,124 @@ function Wrapper({ children }) {
             <span className="text-2xl cursor-pointer ">
               <img src={logo} />
             </span>
-            <span className="text-2xl cursor-pointer md:hidden black">
+            <span
+              className="text-2xl cursor-pointer md:hidden black"
+              onClick={() => handleToggler()}
+            >
               <ion-icon name="menu"></ion-icon>
             </span>
+            {toggle ? (
+              <nav
+                id="sidenav-1"
+                class="absolute left-0 top-0 z-[1035] h-full w-60 -translate-x-full overflow-hidden bg-white shadow-[0_4px_12px_0_rgba(0,0,0,0.07),_0_2px_4px_rgba(0,0,0,0.05)] data-[te-sidenav-hidden='false']:translate-x-0 dark:bg-zinc-800"
+                data-te-sidenav-init
+                data-te-sidenav-hidden="false"
+                data-te-sidenav-position="absolute"
+              >
+                <ul
+                  class="relative m-0 list-none px-[0.2rem]"
+                  data-te-sidenav-menu-ref
+                >
+                  <li class="relative">
+                    <a
+                      class="flex h-12 cursor-pointer items-center truncate rounded-[5px] px-6 py-4 text-[0.875rem] text-gray-600 outline-none transition duration-300 ease-linear hover:bg-slate-50 hover:text-inherit hover:outline-none focus:bg-slate-50 focus:text-inherit focus:outline-none active:bg-slate-50 active:text-inherit active:outline-none data-[te-sidenav-state-active]:text-inherit data-[te-sidenav-state-focus]:outline-none motion-reduce:transition-none dark:text-gray-300 dark:hover:bg-white/10 dark:focus:bg-white/10 dark:active:bg-white/10"
+                      data-te-sidenav-link-ref
+                    >
+                      <span class="mr-4 [&>svg]:h-4 [&>svg]:w-4 [&>svg]:text-gray-400 dark:[&>svg]:text-gray-300">
+                        <FaHome />
+                      </span>
+                      <span>Home</span>
+                    </a>
+                  </li>
+                  <li class="relative">
+                    <a
+                      class="flex h-12 cursor-pointer items-center truncate rounded-[5px] px-6 py-4 text-[0.875rem] text-gray-600 outline-none transition duration-300 ease-linear hover:bg-slate-50 hover:text-inherit hover:outline-none focus:bg-slate-50 focus:text-inherit focus:outline-none active:bg-slate-50 active:text-inherit active:outline-none data-[te-sidenav-state-active]:text-inherit data-[te-sidenav-state-focus]:outline-none motion-reduce:transition-none dark:text-gray-300 dark:hover:bg-white/10 dark:focus:bg-white/10 dark:active:bg-white/10"
+                      data-te-sidenav-link-ref
+                    >
+                      <span class="mr-4 [&>svg]:h-4 [&>svg]:w-4 [&>svg]:text-gray-400 dark:[&>svg]:text-gray-300">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          fill="currentColor"
+                          class="h-4 w-4"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-2.625 6c-.54 0-.828.419-.936.634a1.96 1.96 0 00-.189.866c0 .298.059.605.189.866.108.215.395.634.936.634.54 0 .828-.419.936-.634.13-.26.189-.568.189-.866 0-.298-.059-.605-.189-.866-.108-.215-.395-.634-.936-.634zm4.314.634c.108-.215.395-.634.936-.634.54 0 .828.419.936.634.13.26.189.568.189.866 0 .298-.059.605-.189.866-.108.215-.395.634-.936.634-.54 0-.828-.419-.936-.634a1.96 1.96 0 01-.189-.866c0-.298.059-.605.189-.866zm2.023 6.828a.75.75 0 10-1.06-1.06 3.75 3.75 0 01-5.304 0 .75.75 0 00-1.06 1.06 5.25 5.25 0 007.424 0z"
+                            clip-rule="evenodd"
+                          />
+                        </svg>
+                      </span>
+                      <span>About</span>
+                    </a>
+                  </li>
+                  <li class="relative">
+                    <a
+                      class="flex h-12 cursor-pointer items-center truncate rounded-[5px] px-6 py-4 text-[0.875rem] text-gray-600 outline-none transition duration-300 ease-linear hover:bg-slate-50 hover:text-inherit hover:outline-none focus:bg-slate-50 focus:text-inherit focus:outline-none active:bg-slate-50 active:text-inherit active:outline-none data-[te-sidenav-state-active]:text-inherit data-[te-sidenav-state-focus]:outline-none motion-reduce:transition-none dark:text-gray-300 dark:hover:bg-white/10 dark:focus:bg-white/10 dark:active:bg-white/10"
+                      data-te-sidenav-link-ref
+                    >
+                      <span class="mr-4 [&>svg]:h-4 [&>svg]:w-4 [&>svg]:text-gray-400 dark:[&>svg]:text-gray-300">
+                        {/* <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          fill="currentColor"
+                          class="h-4 w-4"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-2.625 6c-.54 0-.828.419-.936.634a1.96 1.96 0 00-.189.866c0 .298.059.605.189.866.108.215.395.634.936.634.54 0 .828-.419.936-.634.13-.26.189-.568.189-.866 0-.298-.059-.605-.189-.866-.108-.215-.395-.634-.936-.634zm4.314.634c.108-.215.395-.634.936-.634.54 0 .828.419.936.634.13.26.189.568.189.866 0 .298-.059.605-.189.866-.108.215-.395.634-.936.634-.54 0-.828-.419-.936-.634a1.96 1.96 0 01-.189-.866c0-.298.059-.605.189-.866zm2.023 6.828a.75.75 0 10-1.06-1.06 3.75 3.75 0 01-5.304 0 .75.75 0 00-1.06 1.06 5.25 5.25 0 007.424 0z"
+                            clip-rule="evenodd"
+                          />
+                        </svg> */}
+                        <FaAddressBook />
+                      </span>
+                      <span>Contact</span>
+                    </a>
+                  </li>
+                  <li class="relative">
+                    <a
+                      class="flex h-12 cursor-pointer items-center truncate rounded-[5px] px-6 py-4 text-[0.875rem] text-gray-600 outline-none transition duration-300 ease-linear hover:bg-slate-50 hover:text-inherit hover:outline-none focus:bg-slate-50 focus:text-inherit focus:outline-none active:bg-slate-50 active:text-inherit active:outline-none data-[te-sidenav-state-active]:text-inherit data-[te-sidenav-state-focus]:outline-none motion-reduce:transition-none dark:text-gray-300 dark:hover:bg-white/10 dark:focus:bg-white/10 dark:active:bg-white/10"
+                      data-te-sidenav-link-ref
+                    >
+                      <span class="mr-4 [&>svg]:h-4 [&>svg]:w-4 [&>svg]:text-gray-400 dark:[&>svg]:text-gray-300">
+                        {" "}
+                        <FaHandHolding />{" "}
+                      </span>
+                      <span>Services</span>
+                    </a>
+                  </li>
+                  <li class="relative">
+                    <a
+                      class="flex h-12 cursor-pointer items-center truncate rounded-[5px] px-6 py-4 text-[0.875rem] text-gray-600 outline-none transition duration-300 ease-linear hover:bg-slate-50 hover:text-inherit hover:outline-none focus:bg-slate-50 focus:text-inherit focus:outline-none active:bg-slate-50 active:text-inherit active:outline-none data-[te-sidenav-state-active]:text-inherit data-[te-sidenav-state-focus]:outline-none motion-reduce:transition-none dark:text-gray-300 dark:hover:bg-white/10 dark:focus:bg-white/10 dark:active:bg-white/10"
+                      data-te-sidenav-link-ref
+                    >
+                      <span class="mr-4 [&>svg]:h-4 [&>svg]:w-4 [&>svg]:text-gray-400 dark:[&>svg]:text-gray-300">
+                        {" "}
+                        <FaSignInAlt />{" "}
+                      </span>
+                      <span>
+                        {" "}
+                        <Link to="/login">Sign In</Link>
+                      </span>
+                    </a>
+                  </li>
+                  <li class="relative">
+                    <a
+                      class="flex h-12 cursor-pointer items-center truncate rounded-[5px] px-6 py-4 text-[0.875rem] text-gray-600 outline-none transition duration-300 ease-linear hover:bg-slate-50 hover:text-inherit hover:outline-none focus:bg-slate-50 focus:text-inherit focus:outline-none active:bg-slate-50 active:text-inherit active:outline-none data-[te-sidenav-state-active]:text-inherit data-[te-sidenav-state-focus]:outline-none motion-reduce:transition-none dark:text-gray-300 dark:hover:bg-white/10 dark:focus:bg-white/10 dark:active:bg-white/10"
+                      data-te-sidenav-link-ref
+                    >
+                      <span class="mr-4 [&>svg]:h-4 [&>svg]:w-4 [&>svg]:text-gray-400 dark:[&>svg]:text-gray-300">
+                        {" "}
+                        <FaRegistered />{" "}
+                      </span>
+                      <span>
+                        <Link to="/register">Sign Up</Link>
+                      </span>
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            ) : null}
           </div>
           <div className="flex justify-between">
             <ul className="md:flex md:items-center z-[-1] md:z-auto md:static absolute w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500">
