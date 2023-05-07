9a4dd1ab77ef063e4bdbc36db15e64ee5e3b3494
LontchiFrank <fodjof6@gmail.com>
2023-05-07 23:49:10 +0100

    added loader on backend

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,10 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { authSlide } from "../features/authSlice";
 import { poemSlide } from "../features/poemSlice";
 
-export const store = configureStore({
-  reducer: {
-    poems: poemSlide.reducer,
-    user: authSlide.reducer,
-  },
+const rootReducer = combineReducers({
+  poems: poemSlide.reducer,
+  user: authSlide.reducer,
 });
+
+export const store = configureStore({ reducer: rootReducer });
